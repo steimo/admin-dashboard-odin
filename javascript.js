c2dd48655373b88c814e5a63d30573b81028ef4b
steimz <vladyslav.kolomiets@protonmail.com>
2022-02-06 23:22:40 +0200

    First commit

---
+++ javascript.js
@@ -0,0 +1,22 @@
+// const menuIconEl = $('.menu-icon');
+// const sidebarEl = $('.sidebar');
+
+// // Add and remove provided class names
+// function toggleClassName(el, className) {
+//   if (el.hasClass(className)) {
+//     el.removeClass(className);
+//   } else {
+//     el.addClass(className);
+//   }
+// }
+
+// // Open the side nav on click
+// menuIconEl.on('click', function() {
+//   toggleClassName(sidebarEl, 'active');
+// 	console.log('fds')
+// });
+
+// // // Close the side nav on click
+// // sidenavCloseEl.on('click', function() {
+// //   toggleClassName(sidebarEl, 'active');
+// // });
